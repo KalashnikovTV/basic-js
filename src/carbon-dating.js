0cc7b75dd5d26573c4902f59940579865e651c5c
KalashnikovTV <max.malaschkov@mail.ru>
2021-01-27 18:22:58 +0300

    feat: all tasks completed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if(typeof +sampleActivity != Number){
+    return false;
+  }
+
+  let ACTIVITIES_RATIO = MODERN_ACTIVITY / sampleActivity;
+  let CONSTANT_RATE = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil((Math.log(ACTIVITIES_RATIO) / CONSTANT_RATE));
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+  let number = 0;
+
+  array.forEach(el => {
+	  el.forEach(elem => {
+      if (elem === '^^') {
+        number++;
+      }
+    });
+  });
+
+  return number;
 };
